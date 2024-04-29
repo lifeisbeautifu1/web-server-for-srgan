3c9bab088ab402a4f40215a98679d45f83413982
lifeisbeautiful <poltoradnevone@gmail.com>
2024-04-29 12:09:34 +0400

    feat: added test route

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+    return 'Hi!'
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
