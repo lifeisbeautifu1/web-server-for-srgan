47b2e6e5dfa3f330fd97e0b2ca6628a17f59dc72
lifeisbeautiful <poltoradnevone@gmail.com>
2024-05-03 21:07:24 +0400

    feat: allow jpg format, added jpg artifacts removal

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, send_file
 from flask_cors import cross_origin
 from PIL import Image
-from utils import allowed_file, serve_pil_image, upscale
+from utils import allowed_file, serve_pil_image, upscale, need_preprocessing, process_jpg
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 app = Flask(__name__)
 
-@app.route('/api/upload', methods = ['POST'])
 @cross_origin(origins=['https://super-resolution-with-srgan.netlify.app'])
+@app.route('/api/upload', methods = ['POST'])
 def upload_file():
     if 'photo' not in request.files:
         return 'Forgot to send photo?', 400
@@ -19,10 +21,14 @@ def upload_file():
     if photo and allowed_file(photo.filename):
         # Do the magic.
         low_res_image = Image.open(photo)
+
+        if need_preprocessing(photo.filename):
+            low_res_image = process_jpg(low_res_image)
+
         high_res_image = upscale(low_res_image)
         return serve_pil_image(high_res_image)
     else:
-        return "Only png's are allowed", 400
+        return "Only png's and jpg's are allowed", 400
 
 @app.route('/')
 def hello_world():
